4b783cc55a12bee96d74f40727eedc8d1d25a0b1
John Murphy <chiefsmurph@gmail.com>
2017-08-19 19:27:39 -0700

    leaderboard

--- server/leaderboard.js
+++ server/leaderboard.js
@@ -3,46 +3,50 @@ var Songs = require('./models/songs');
 var Fetches = require('./models/fetches');
 var async = require('async');
 
+
+var getLeaderboard = (mainCb) => {
+
+  let topSongs, topPlaylists, topFetches;
+  async.waterfall([
+    function(cb) {
+      Songs.getTopSongs(res => {
+        topSongs = res;
+        console.log('top songs', topSongs);
+        cb();
+      });
+    },
+    function(cb) {
+      Playlists.getTopPlaylists(res => {
+        topPlaylists = res;
+        console.log('top pls', topPlaylists);
+        cb();
+      });
+    },
+    function(cb) {
+      Fetches.getTopFetches(res => {
+        topFetches = res;
+        console.log('top fetches', topFetches);
+        cb();
+      })
+    },
+    function(cb) {
+      mainCb({
+        topSongs,
+        topPlaylists,
+        topFetches
+      });
+    }
+  ]);
+
+};
+
+
 module.exports = {
   broadcast: (emit) => {
-    this.getLeaderboard(leaderboard => {
+    getLeaderboard(leaderboard => {
       console.log('broadcasting leaderboard');
       emit('leaderboard', leaderboard);
     });
   },
-  getLeaderboard: (mainCb) => {
-
-    let topSongs, topPlaylists, topFetches;
-    async.waterfall([
-      function(cb) {
-        Songs.getTopSongs(res => {
-          topSongs = res;
-          console.log('top songs', topSongs);
-          cb();
-        });
-      },
-      function(cb) {
-        Playlists.getTopPlaylists(res => {
-          topPlaylists = res;
-          console.log('top pls', topPlaylists);
-          cb();
-        });
-      },
-      function(cb) {
-        Fetches.getTopFetches(res => {
-          topFetches = res;
-          console.log('top fetches', topFetches);
-          cb();
-        })
-      },
-      function(cb) {
-        mainCb({
-          topSongs,
-          topPlaylists,
-          topFetches
-        });
-      }
-    ]);
-
-  }
-}
+  getLeaderboard: getLeaderboard
+};
